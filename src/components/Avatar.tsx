2e0199d5b1c602d66726199f833d8a8ca4b10e53
Feisal Ahmed <feisala132@gmail.com>
2024-12-04 17:43:31 +0000

    Adding avatars

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,15 @@
+import { Avatar } from "@/components/ui/avatar"
+
+export const Epictetus = () => {
+  return <Avatar name="Epictetus" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcommunity.thriveglobal.com%2Fwp-content%2Fuploads%2F2020%2F03%2FScreen-Shot-2020-03-02-at-1.05.47-PM.png&f=1&nofb=1&ipt=1b1bc65268729ff8440cf37ffdad76496806439d672e3e9cf8a3646aa7c48080&ipo=images" />
+}
+
+export const Marcus = () => {
+  return <Avatar name="Marcus" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F2.bp.blogspot.com%2F-PRJ5zBck0DI%2FVyCp8HhleqI%2FAAAAAAAAC7U%2FGWcifLJIy-QLoWbsQaBjlXgywFiq2sImQCLcB%2Fs1600%2FEmperor%252BMarcus%252BAurelius.jpg&f=1&nofb=1&ipt=cafd47984e814ecd9b5ff516efd41f2117064b934384a059c1969875b5ab2340&ipo=images" />
+}
+
+
+export const Seneca = () => {
+  return <Avatar name="Seneca" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.oGOAUz3Mh7k8tJzACgqDYQHaLH%26pid%3DApi&f=1&ipt=6e2549def068933d051e7494b74311a4b11a2647b023ccfde12c4aef268f1079&ipo=images" />
+}
+
